0a559b8fcf09196582674a0808529d04e3ffdd6c
sahil491 <sahusahil02@gmail.com>
2022-01-12 12:23:23 +0530

    my first commit

---
+++ 5qus.js
@@ -0,0 +1,49 @@
+// voter aligiable test
+
+// var age = 15;
+// if (age >=18)
+// console.log("you are aligiable for voting")
+// else 
+// console.log("you are not aligiable for voting")
+
+// percentage program 
+
+// var a = 32;
+// if (a >= 60 && a<=100)
+// console.log("first division")
+// else if (a >= 50 && a<60)
+// console.log("second division")
+// else if (a >=33 && a<50)
+// console.log("third division")
+// else
+// console.log("fail")
+
+// compare 2 int and find the greater one
+
+// var a = 40;
+// var b = 40;
+// if (a > b)
+// console.log("first no. is greater ")
+// else if (a == b)
+// console.log("both No. are equal")
+// else 
+// console.log("second no. is greater")
+
+// compare 3 int and find the greater one
+
+// var a = 1000;
+// var b = 2000;
+// var c = 60;
+// if (a>b && a>c)
+// console.log("A number is greater")
+// else if(b>a && b>c)
+// console.log("B number is greater")
+// else if (c>b && c>a)
+// console.log("C numbers is greater")
+// else 
+// console.log("all number are equal")
+// to add the two number
+var a = 20;
+var b = 60;
+var c = (a+b);
+console.log(c)
